6322158f54b764ad8716cec1453cd5ce4d634d26
remiv1 <remiv1@gmail.com>
2026-01-20 17:43:22 +0100

    feat: Mise à jour des fonctions d'encapsulation et de décapsulation pour inclure le nonce et le ciphertext

--- src/n1/vpn1_multiscreen/tunnel.py
+++ src/n1/vpn1_multiscreen/tunnel.py
@@ -6,18 +6,21 @@ Gestion du tunnel VPN.
 import json
 from typing import Any, Dict
 
-def encapsulate(payload: bytes, tunnel_type: str="MINI-VPN"):
+def encapsulate(nonce: bytes, ciphertext: bytes, tunnel_type: str="MINI-VPN"):
     """Encapsule les données dans un paquet JSON pour le tunnel VPN."""
     if isinstance(tunnel_type, bytes):
         tunnel_type = tunnel_type.hex()
     packet: Dict[str, Any] = {
         "type": tunnel_type,
-        "length": len(payload),
-        "payload": payload.hex()
+        "nonce": nonce.hex(),
+        "length": len(ciphertext),
+        "payload": ciphertext.hex()
     }
     return json.dumps(packet).encode()
 
 def decapsulate(packet_bytes: bytes):
     """Décapsule les données d'un paquet JSON du tunnel VPN."""
     packet = json.loads(packet_bytes.decode())
-    return bytes.fromhex(packet["payload"])
+    nonce = bytes.fromhex(packet["nonce"])
+    ciphertext = bytes.fromhex(packet["payload"])
+    return nonce, ciphertext
